7afed2394882fde11b972574dcb81870949cec49
lzkelley <lzkelley@gmail.com>
2020-06-23 08:56:47 -0500

    Updates

--- __init__.py
+++ __init__.py
@@ -2,19 +2,25 @@
 """
 
 import os
+import glob
 import sys
-import socket 
+from datetime import datetime
+import socket
+
+import numpy as np
+
+import zcode.inout as zio
+import zcode.plot as zplot
 
 _CDIR = os.path.split(os.path.abspath(__file__))[0]
 
-_PATH_OUTPUT = "analysis_output"
-PATH_OUTPUT = os.path.join(_CDIR, _PATH_OUTPUT)
+# DNAME_ANALYSIS_OUTPUT = "analysis"
 
-_check_paths = [PATH_OUTPUT]
+from . import readio  # noqa
+from . import plot  # noqa
 
-# Determine Hostname (i.e. host system) and set host-dependent parameters
-# -------------------------------------------------------------------------------
 
+'''
 _hostname = socket.gethostname().lower()
 ILLUSTRIS_PYTHON_PATH = None
 if 'daedalus' in _hostname:
@@ -33,12 +39,11 @@ else:
     print("WARNING: unrecognized host!")
 
 # Add Path to `illustris_python` module
-'''
-ILLUSTRIS_PYTHON_PATH = "/n/home00/lkelley/illustris/"
-if ILLUSTRIS_PYTHON_PATH not in sys.path:
-    sys.path.append(ILLUSTRIS_PYTHON_PATH)
-    print("Added '{}' `to sys.path`".format(ILLUSTRIS_PYTHON_PATH))
-'''
+# ILLUSTRIS_PYTHON_PATH = "/n/home00/lkelley/illustris/"
+# if ILLUSTRIS_PYTHON_PATH not in sys.path:
+#     sys.path.append(ILLUSTRIS_PYTHON_PATH)
+#     print("Added '{}' `to sys.path`".format(ILLUSTRIS_PYTHON_PATH))
+
 if (ILLUSTRIS_PYTHON_PATH is not None) and (ILLUSTRIS_PYTHON_PATH not in sys.path):
     if os.path.exists(ILLUSTRIS_PYTHON_PATH):
         sys.path.append(ILLUSTRIS_PYTHON_PATH)
@@ -51,3 +56,285 @@ for path in _check_paths:
     if not os.path.exists(path):
         os.makedirs(path)
         print("Created '{}'".format(path))
+'''
+
+
+class Arepo_Sim_Path:
+
+    _DNAME_OUTPUT = "output"
+    _DNAME_ANALYSIS = "analysis"
+
+    def __init__(self, sim_path, create=True):
+        # Make sure the path does NOT include a terminal path-separator
+        sim_path = os.path.dirname(os.path.join(sim_path, ''))
+        # If the 'output' folder is specified, move up a directory to the base-path
+        if sim_path.endswith(self._DNAME_OUTPUT):
+            sim_path = os.path.dirname(sim_path)
+
+        self.sim = sim_path
+        self.output = os.path.join(sim_path, self._DNAME_OUTPUT)
+        self.analysis = os.path.join(sim_path, self._DNAME_ANALYSIS)
+
+        _check_paths = [self.output, self.analysis]
+        if create:
+            for cp in _check_paths:
+                zio.check_path(cp)
+
+        return
+
+    def __str__(self):
+        return self.sim
+
+    def snapshots(self, snap=None):
+        if snap is None:
+            fnames = snap_files(self.output)
+        else:
+            if np.isscalar(snap):
+                squeeze = True
+            snap = np.atleast_1d(snap)
+            fnames = [os.path.join(self.output, "snap_{:03d}.hdf5".format(sn))
+                      for sn in snap]
+            if squeeze:
+                fnames = fnames[0]
+
+        return fnames
+
+    def num_snaps(self):
+        return len(self.snapshots())
+
+    def fname(self, fname, path=None, subdir=None, snap_num=None, modify=False):
+        if path is None:
+            path = self.analysis
+
+        if subdir is True:
+            subdir = fname.split('.')[0]
+
+        if subdir is not None:
+            path = os.path.join(path, subdir)
+
+        # Try to 'format' the snap_num in place (e.g. "voronoi_mesh_{:03d}" ==> "voronoi_mesh_023")
+        #   otherwise, append snap (e.g. "voronoi_mesh" ==> "voronoi_mesh_snap-023")
+        if snap_num is not None:
+            import string
+            if string.Formatter().parse(fname)[1] is not None:
+                fname = fname.format(snap_num)
+            else:
+                fname = fname_append_snap(fname, snap_num)
+
+        fname = os.path.join(path, fname)
+
+        if modify:
+            fname = zio.modify_exists(fname)
+
+        return fname
+
+    def save_fig(self, fig, fname, path=None, subdir=None, snap_num=None, modify=False,
+                 verbose=False, **kwargs):
+        fname = self.fname(
+            fname, path=path, subdir=subdir, snap_num=snap_num, modify=modify)
+        kwargs.setdefault('transparent', True)
+        kwargs.setdefault('dpi', 100)
+        zio.check_path(fname)
+        fig.savefig(fname, **kwargs)
+        if verbose:
+            print("saved to '{}' size: {}".format(fname, zio.get_file_size(fname)))
+        return fname
+
+    '''
+    def snap_file(self, fname, snap_num=None):
+        if snap_num is not None:
+            fname = fname.format(snap_num)
+
+        if not os.path.exists(fname):
+            raise ValueError("Path '{}' does not exist!".format(fname))
+
+        if os.path.isdir(fname):
+            if snap_num is None:
+                raise ValueError("If path is provided, `snap_num` must be given!")
+            fname = os.path.join(fname, pattern.format(snap_num))
+
+        if not os.path.exists(fname):
+            raise ValueError("File '{}' does not exist!".format(fname))
+
+        return fname
+    '''
+
+
+def _snap_file_from_path(fname, pattern, snap_num=None):
+    if not os.path.exists(fname):
+        raise ValueError("Path '{}' does not exist!".format(fname))
+
+    if os.path.isdir(fname):
+        if snap_num is None:
+            raise ValueError("If path is provided, `snap_num` must be given!")
+        fname = os.path.join(fname, pattern.format(snap_num))
+
+    if not os.path.exists(fname):
+        raise ValueError("File '{}' does not exist!".format(fname))
+
+    return fname
+
+
+def snap_files(path):
+    pattern = "snap_*.hdf5"
+    fname = os.path.join(path, pattern)
+    snaps = sorted(glob.glob(fname))
+    return snaps
+
+
+'''
+def path_analysis_from_output(path_output):
+    if os.path.basename(path_output).lower() != 'output':
+        raise ValueError("Path doesn't look right!  '{}'".format(path_output))
+
+    path = os.path.join(path_output, os.path.pardir, DNAME_ANALYSIS_OUTPUT, '')
+    return path
+'''
+
+'''
+def get_num_snaps(arepo_output_dir):
+    # Get number of snapshots
+    pattern = os.path.join(arepo_output_dir, "snapdir_*")
+    snap_fnames = sorted(glob.glob(pattern))
+    if len(snap_fnames) < 1:
+        print("Pattern: '{}'".format(pattern))
+        print("Directory contents: ", os.listdir(arepo_output_dir))
+        raise RuntimeError("No snapshot directories found!")
+
+    num_snaps = len(snap_fnames)
+    return num_snaps
+'''
+
+
+def fname_append_snap(fname, snap):
+    if isinstance(snap, str):
+        app = "_snap-{}".format(snap)
+    else:
+        app = "_snap-{:03d}".format(snap)
+
+    fname = zio.modify_filename(fname, append=app)
+    return fname
+
+
+'''
+def save_fig(fig, fname, path=None, subdir=None, snap_num=None, modify=False,
+             transparent=True, dpi=100, **kwargs):
+
+    # if os.path.basename(path).lower() == 'output':
+    #    path = os.path.join(path, os.path.pardir, DIR_ANALYSIS_OUTPUT, '')
+    try:
+        path = path_analysis_from_output(path)
+    except:
+        pass
+
+    if (snap_num is not None) and (subdir is None):
+        comps = fname.split('.')
+        if len(comps) != 2:
+            raise ValueError("Bad `fname` = '{}'  ({})!".format(fname, comps))
+        subdir = comps[0]
+        fname = subdir + "_snap-{:03d}".format(snap_num) + "." + comps[1]
+
+    fname = zplot.save_fig(fig, fname, path=path, modify=modify,
+                           subdir=subdir, transparent=transparent, dpi=dpi, **kwargs)
+    return fname
+'''
+
+
+def main(arepo_sim_dir, snap=-1):
+    arepo_output_dir = os.path.join(arepo_sim_dir, "output", "")
+    sim_name = os.path.split(arepo_sim_dir)[-1]
+    print("Sim name: '{}'".format(sim_name))
+    print("Running on directory: '{}'".format(arepo_output_dir))
+    num_snaps = get_num_snaps(arepo_output_dir)
+    print("\t{} Snapshots".format(num_snaps))
+
+    # print(os.listdir(arepo_output_dir))
+    # image_slices(arepo_output_dir)
+    if snap < 0:
+        snap = num_snaps + snap
+
+    subfind_at_snapshot(arepo_output_dir, snap, sim_name)
+
+    return
+
+
+def subfind_at_snapshot(arepo_output_dir, snapshot, sim_name):
+    beg = datetime.now()
+    subhalos = ill.groupcat.loadSubhalos(arepo_output_dir, snapshot)
+    end = datetime.now()
+    dur = (end - beg)
+    print("Loaded {} subhalos after {}".format(subhalos['count'], str(dur)))
+    print("\tkeys: '{}'\n".format(subhalos.keys()))
+
+    fig = plot.stars_galaxies(subhalos, npart_select=False)
+    save_fig(fig, 'subhalos_galaxies.pdf', sim_name)
+
+    fig = plot.stars_galaxies(subhalos, npart_select=True)
+    save_fig(fig, 'subhalos_galaxies_{}.pdf'.format(plot.part_cut_str), sim_name)
+
+    fig = plot.num_particles(subhalos)
+    save_fig(fig, 'subhalos_num-parts_{}.pdf'.format(plot.part_cut_str), sim_name)
+
+    fig = plot.blackhole_galaxy(subhalos, npart_select=False)
+    save_fig(fig, 'subhalos_blackholes-hosts.pdf', sim_name)
+
+    fig = plot.blackhole_galaxy(subhalos, npart_select=True)
+    save_fig(fig, 'subhalos_blackholes-hosts_{}.pdf'.format(plot.part_cut_str), sim_name)
+    return
+
+
+def image_slices(arepo_output_dir, dest=".", fname="density_field_",
+                 log_flag=False, interp='nearest'):
+    """Draw the image slices produced directly by Arepo (e.g. 'proj_density_field_000').
+
+    These slices are enabled with the `VORONOI_IMAGES_FOREACHSNAPSHOT` option.
+
+    """
+    # fname = "proj_density_field_"
+    # fname = "density_slice_"    # for some reason, some of these values are negative?
+    # fname = "metal_slice_"
+    # fname = "chemelements_slice_"
+    pattern = os.path.join(arepo_output_dir, fname + "*")
+    files = sorted(glob.glob(pattern))
+    print("Found '{}' files matching pattern '{}'".format(len(files), pattern))
+
+    DOUBLE_TYPE = np.float32
+    INT_TYPE = np.int32
+
+    path_output = os.path.realpath(dest)
+    print("Outputting to '{}'".format(path_output))
+
+    # for fname in [files[-1]]:
+    for fname in files:
+        fnum = os.path.split(fname)[-1].split("_")[-1]
+
+        with open(fname, 'rb') as data:
+            # Read header
+            nums = np.fromfile(data, INT_TYPE, 2)     # read two 32 bit integers
+            print("num = ", nums)
+            # Read density
+            dens = np.fromfile(data, DOUBLE_TYPE, np.product(nums)).reshape(nums)
+            print("density extr = ", np.min(dens), np.max(dens))
+
+        fig, ax = plt.subplots(figsize=[10, 10])
+        vals = np.log10(dens) if log_flag else dens
+        ax.imshow(vals, interpolation=interp)
+        # fname_out = '{}{}.pdf'.format(fname, fnum)
+        fname_out = '{}.pdf'.format(os.path.basename(fname))
+        # fname_out = os.path.join(PATH_OUTPUT, fname_out)
+        fname_out = os.path.join(path_output, fname_out)
+        fig.savefig(fname_out)
+        print("Saved to '{}'".format(fname_out))
+        plt.close('all')
+
+    return
+
+
+if __name__ == "__main__":
+    print(sys.argv)
+    if len(sys.argv) > 1:
+        arepo_sim_dir = sys.argv[1]
+    else:
+        raise RuntimeError("No directory provided!")
+
+    main(arepo_sim_dir)
--- plot.py
+++ plot.py
@@ -1,11 +1,28 @@
 """
 """
 
+import os
+import glob
+import subprocess
+import logging
+
 import numpy as np
-# import matplotlib as mpl
+import matplotlib as mpl
+import matplotlib.patches   # noqa
 import matplotlib.pyplot as plt
+import scipy as sp
+import scipy.stats  # noqa
+
+from numba import njit
+import tqdm.notebook as tqdm
+
+import zcode.plot as zplot
+import zcode.math as zmath
 
 from .const import PARTS, CONV_ILL_TO_SOL
+
+import arepo_analysis as lysis
+from . import readio
 
 ZSOL = 0.012
 
@@ -283,3 +300,381 @@ def label_part_cut(fig, ax):
             horizontalalignment='center', verticalalignment='top',
             transform=fig.transFigure)
     return
+
+
+# ===========================  NEW STUFF ========================================
+
+
+def rad_proj_from_2d(path, snap_num, scatter_param='Density', hist_param='Masses',
+                     hist_dens=True, extr=None, nbins=20):
+    bin_sigma = 3
+
+    _params = ['Coordinates', 'Masses']
+    params = [pp for pp in _params]
+    if (scatter_param is not None) and (scatter_param not in _params):
+        params.append(scatter_param)
+    if (hist_param is not None) and (hist_param not in _params):
+        params.append(hist_param)
+
+    snap = lysis.readio.snapshot(path, snap_num=snap_num, pars=params)
+    pos = snap['Coordinates']
+    mass = snap['Masses']
+
+    scat = None
+    if scatter_param is not None:
+        scat = snap[scatter_param]
+    hist = None
+    if hist_param is not None:
+        hist = snap[hist_param]
+
+    com = np.sum(pos*mass[:, np.newaxis], axis=0) / np.sum(mass)
+    rads = np.linalg.norm(pos - com, axis=-1)
+    if extr is None:
+        extr = zmath.quantiles(rads, sigmas=[-bin_sigma, bin_sigma])
+
+    edges = None
+    if hist is not None:
+        stat = 'sum' if hist_dens else 'mean'
+
+        edges = zmath.spacing(extr, 'log', nbins+1)
+        hist, *_ = sp.stats.binned_statistic(rads, hist, statistic=stat, bins=edges)
+        if hist_dens:
+            da = np.diff(np.pi*edges**2)
+            hist = hist / da
+
+    return rads, scat, hist, edges
+
+
+def plot_proj_1d(path, scatter_param='Density', hist_param='Masses', snaps=None, hist_dens=True,
+                 extr=None, nbins=20, xlim=None, ylim=None, movie=True, framerate=None):
+    num_snaps = len(lysis.readio.snap_files(path))
+    fname = "proj1d_{}-{}.png".format(scatter_param.lower(), hist_param.lower())
+
+    init = None
+    mean = None
+    if snaps is None:
+        snaps = range(num_snaps)
+
+    yextr = None
+    for snap in tqdm.tqdm(snaps):
+        rads, scat, hist, edges = lysis.plot.rad_proj_from_2d(
+            path, snap, scatter_param=scatter_param, hist_param=hist_param,
+            hist_dens=hist_dens, extr=extr, nbins=nbins)
+        if extr is None:
+            extr = zmath.minmax(edges)
+        if init is None:
+            init = np.copy(hist)
+
+        idx = (hist > 0.0)
+        if mean is None:
+            mean = np.zeros_like(hist)
+            mean[idx] = hist[idx]
+        else:
+            mean[idx] = mean[idx] + hist[idx]
+
+        yextr = zmath.minmax(scat, prev=yextr, log_stretch=0.1, filter='>')
+
+    mean = mean / num_snaps
+    if ylim is None:
+        ylim = yextr
+
+    output_fnames = []
+    for snap_num in tqdm.tqdm(snaps):
+        fig = lysis.plot.plot_proj_1d_snap(
+            path, snap_num, scatter_param=scatter_param, hist_param=hist_param,
+            extr=extr, hist_dens=hist_dens, mean=mean, init=init)
+
+        fig.axes[0].set(xlim=xlim, ylim=ylim)
+        _fname = lysis.save_fig(fig, fname, path, snap_num=snap_num, verbose=False)
+        if _fname is None:
+            print("BAD!")
+            print(_fname, fname)
+            raise RuntimeError()
+
+        output_fnames.append(_fname)
+        plt.close('all')
+
+    if framerate is None:
+        framerate = len(snaps) / 20
+        framerate = int(np.clip(framerate, 2, 15))
+
+    print("Saved to (e.g.) '{}'".format(output_fnames[0]))
+    if movie:
+        movie_fname = make_movie(path, output_fnames, fname.replace('.png', '.mp4'),
+                                 framerate=framerate)
+        print("Saved movie to '{}'".format(movie_fname))
+
+    return
+
+
+def plot_proj_1d_snap(path, snap_num, scatter_param='Density', hist_param='Masses',
+                      mean=None, init=None, hist_dens=True, extr=None, ave=None, nbins=20):
+
+    fig, ax = zplot.figax(left=0.05, right=0.98, bottom=0.1, top=0.98)
+    keys, head, pars = lysis.readio.snapshot(path, snap_num=snap_num, header=True)
+    lysis.plot.draw_snap_time(ax, head, num=snap_num)
+
+    rads, scat, hist, edges = rad_proj_from_2d(
+        path, snap_num, scatter_param=scatter_param, hist_param=hist_param,
+        hist_dens=hist_dens, extr=extr, nbins=nbins)
+
+    if hist is not None:
+        zplot.plot_hist_line(ax, edges, hist, color='r', zorder=10, alpha=0.6, lw=2.0)
+
+    if mean is not None:
+        zplot.plot_hist_line(ax, edges, mean, color='r', ls='--', zorder=10, alpha=0.6, lw=2.0)
+
+    if init is not None:
+        zplot.plot_hist_line(ax, edges, init, color='k', ls='--', zorder=10, alpha=0.3, lw=2.0)
+
+    if scat is not None:
+        ax.scatter(rads, scat, alpha=0.4, s=10, facecolor='b')
+
+    return fig
+
+
+def plot_mesh_2d(path, snap_num=0, param='Density', parse_param=None,
+                 region=None, periodic=None, smap=None):
+
+    try:
+        path = path.output
+    except:
+        path = str(path)
+
+    mesh = readio.voronoi_mesh(path, snap_num=snap_num)
+    # snap = readio.snapshot(path_input, snap_num=snap_num, pars=['Coordinates', param])
+    # pos = snap['Coordinates']
+    snap = readio.snapshot(path, snap_num=snap_num, pars=[param])
+    # pos = snap['Coordinates']
+    vals = snap[param]
+    if parse_param is not None:
+        vals = parse_param(vals)
+
+    fig, ax = plt.subplots(figsize=[12, 10])
+    plt.subplots_adjust(left=0.05, right=0.95, bottom=0.0, top=0.98)
+    smap, extr = draw_mesh(
+        ax, mesh, vals=vals, fix_poly=True, lines_flag=True,
+        lw=0.25, color='k', alpha=0.25, region=region, periodic=periodic, smap=smap)
+
+    # ax.scatter(*pos[:, :2].T, color='r', s=2, alpha=0.25)
+    if region is None and periodic is not None:
+        region = periodic
+    if region is not None:
+        ax.set(xlim=region[0], ylim=region[1])
+
+    plt.colorbar(smap, ax=ax, orientation='horizontal', pad=0.05)
+    return fig
+
+
+def draw_mesh(ax, mesh, vals=None, fix_poly=False,
+              smap=None, region=None, periodic=None, lines_flag=True, **kwargs):
+
+    if (not lines_flag) and (vals is None):
+        raise ValueError("Nothing is being plotted!")
+
+    ndt = mesh['ndt_tot']
+    xyz = mesh['xyz_edges']
+    edge_list = mesh['edge_list']
+    nedge_offset = mesh['nedge_offset']
+    nedges = mesh['nedges']
+    NDIM = 2
+    NSIDE_MAX = 20
+
+    xyz = xyz.reshape(ndt, NDIM)
+
+    poly = np.zeros((NSIDE_MAX, NDIM))
+    if lines_flag:
+        lines = np.full((2*len(edge_list), NDIM), np.nan)
+    tot_num = len(nedge_offset)
+
+    mult = 1 if (periodic is None) else 2
+
+    if periodic is not None:
+        periodic = [np.array(pp) if pp is not None else pp for pp in periodic]
+        # np.atleast_2d(periodic)
+    if region is not None:
+        # region = [np.array(pp) if pp is not None else pp for pp in region]
+        region = np.atleast_2d(region)
+
+    if vals is not None:
+        patches = np.empty(mult*tot_num, dtype=object)
+        colors = np.zeros(mult*tot_num)
+
+    cnt = 0
+    valid = np.zeros(mult*tot_num, dtype=bool)
+
+    def add_cell(ee, ne, poly, cnt, end=False):
+        if end:
+            ff = mult*tot_num - 1 - ee
+        else:
+            ff = ee
+
+        if lines_flag:
+            lines[cnt:cnt+ne, :] = poly[:ne, :]
+
+        if vals is not None:
+            inc = 0
+            if fix_poly and np.allclose(poly[0, :], poly[ne-1, :]):
+                ne = ne - 1
+                inc = 1
+
+            pat = mpl.patches.Polygon(poly[:ne])
+            patches[ff] = pat
+            colors[ff] = vals[ee]
+            ne = ne + inc
+
+        valid[ff] = True
+        cnt = cnt + ne + 1
+        return cnt
+
+    pers = 0
+    for ee in tqdm.tqdm(range(tot_num), total=tot_num, leave=False):
+        oo = nedge_offset[ee]
+        ne = nedges[ee]
+        ll = edge_list[oo]
+        lo = xyz[ll]
+        poly[0] = lo
+        if ne >= NSIDE_MAX:
+            err = "Number of edges for element {} = {}, exceeds max {}!".format(ee, ne, NSIDE_MAX)
+            raise ValueError(err)
+
+        for ff in range(1, ne):
+            hh = edge_list[oo+ff]
+            hi = xyz[hh]
+            poly[ff] = hi
+
+        if (region is not None) and (not any_within(poly[:ne], region)):
+            continue
+
+        cnt = add_cell(ee, ne, poly, cnt, end=False)
+
+        if periodic is None:
+            continue
+
+        for dd in range(NDIM):
+            if periodic[dd] is None:
+                continue
+
+            if np.any((poly[:ne, dd] < periodic[dd][0])):
+                dup = np.copy(poly[:ne, :])
+                dup[:, dd] += (periodic[dd][1] - periodic[dd][0])
+                cnt = add_cell(ee, ne, dup, cnt, end=True)
+                pers += 1
+            elif np.any(poly[:ne, dd] > periodic[dd][1]):
+                dup = np.copy(poly[:ne, :])
+                dup[:, dd] -= (periodic[dd][1] - periodic[dd][0])
+                cnt = add_cell(ee, ne, dup, cnt, end=True)
+                pers += 1
+
+        # if cnt > 1000:
+        #     break
+
+    extr = zmath.minmax(colors[valid])
+    if vals is not None:
+        if smap is None:
+            smap = zplot.smap(extr, cmap='viridis')
+
+        p = mpl.collections.PatchCollection(patches[valid], cmap=smap.cmap, norm=smap.norm)
+        p.set_array(colors[valid])
+        ax.add_collection(p)
+
+    if lines_flag:
+        lines = lines[:cnt, :].T
+        ax.plot(*lines, **kwargs)
+
+    return smap, extr
+
+
+def make_movie(path, fname, output, framerate=10):
+    '''
+    try:
+        path_out = lysis.path_analysis_from_output(path)
+    except ValueError:
+        path_out = path
+    '''
+    try:
+        path = path.output
+    except:
+        path = str(path)
+
+    # If `fname` is not a list of complete-filenames then try to glob them by appending 'snap-*'
+    if not isinstance(fname, list):
+        fname = lysis.fname_append_snap(fname, '*')
+        file_list = sorted(glob.glob(fname))
+        if len(file_list) == 0:
+            fname = lysis.fname_append_snap(os.path.join(path, fname), '*')
+            file_list = sorted(glob.glob(fname))
+    else:
+        file_list = fname
+
+    if len(file_list) < 2:
+        raise RuntimeError("Forming file_list failed!")
+
+    file_list_fname = "_temp_file_list.txt"
+    with open(file_list_fname, 'w') as out:
+        for fn in file_list:
+            out.write("file " + fn + "\n")
+
+    otpath = os.path.join(path, output)
+    cmd = [
+        'ffmpeg',
+        '-f', 'concat',
+        '-safe', '0',
+        '-r', str(framerate),
+        '-i', file_list_fname,  # inpath,
+        '-r', str(framerate),
+        '-pix_fmt', 'yuv420p', otpath, '-y'
+    ]
+    try:
+        print(" ".join(cmd))
+        output = subprocess.check_output(cmd)  # , stderr=logging.error)
+    except Exception as err:
+        logging.error("`plot.py:movie()` failed!  command: '{}'".format(" ".join(cmd)))
+        logging.error(str(output))
+        logging.error(str(err))
+        return None
+    finally:
+        os.remove(file_list_fname)
+        pass
+
+    return otpath
+
+
+def draw_snap_time(ax, head, num=None, format='5.1f', **kwargs):
+    time = float(head['Time'])
+    label = "{{:{:}}}".format(format)
+    label = label.format(time)
+    if num is not None:
+        label = "{:03d}: {}".format(num, label)
+
+    kwargs.setdefault('loc', 'ur')
+    kwargs.setdefault('fontsize', 12)
+    kwargs.setdefault('alpha', 0.5)
+    return zplot.text(ax, label, **kwargs)
+
+
+@njit()
+def any_within(poly, bounds):
+    pnts, dims = np.shape(poly)
+    for ii in range(pnts):
+        test = 0
+        for jj in range(dims):
+            if ((poly[ii, jj] > bounds[jj, 0]) and (poly[ii, jj] < bounds[jj, 1])):
+                test += 1
+        if test == dims:
+            return True
+
+    return False
+
+
+@njit()
+def any_without(poly, bounds):
+    pnts, dims = np.shape(poly)
+    for ii in range(pnts):
+        for jj in range(dims):
+            if (poly[ii, jj] < bounds[jj, 0]):
+                return True
+            if (bounds[jj, 1] < poly[ii, jj]):
+                return True
+
+    return False
